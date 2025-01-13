c8e1720fc5990751e65823c71517e5d4d5757399
Krenar Darlishta <48763668+Krenar123@users.noreply.github.com>
2025-01-13 22:08:20 +0100

    changes

--- evaluate/test_instructions.py
+++ evaluate/test_instructions.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-question = "If you had a time machine, but could only go to the past or the future once and never return, which would you choose and why?"
+question = "What are the standards of student behavior?"
 generator = pipeline("text-generation", model="krenard/mistral-automated-qapairs-finetuned-instructions", device="cuda")
 output = generator([{"role": "user", "content": question}], max_new_tokens=128, return_full_text=False)[0]
 print(output["generated_text"])
