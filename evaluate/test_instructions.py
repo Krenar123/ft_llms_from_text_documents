02154b9dedeb699463ea739875a75dec63099926
Krenar Darlishta <48763668+Krenar123@users.noreply.github.com>
2025-01-14 08:43:17 +0100

    changes

--- evaluate/test_instructions.py
+++ evaluate/test_instructions.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
 question = "How does SEEU address non-academic misconduct, such as theft or vandalism?"
-generator = pipeline("text-generation", model="krenard/llama3-2-automated-qapairs-finetuned", device="cuda")
+generator = pipeline("text-generation", model="krenard/llama3-2-automated-qapairs-finetuned-instructions", device="cuda")
 output = generator([{"role": "user", "content": question}], max_new_tokens=128, return_full_text=False)[0]
 print(output["generated_text"])
