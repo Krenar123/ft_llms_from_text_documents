39c394f4bfe99745c1735e33bdec31a10c39d7b0
Krenar Darlishta <48763668+Krenar123@users.noreply.github.com>
2025-01-18 13:26:36 +0100

    ch

--- evaluate/test_llama2.py
+++ evaluate/test_llama2.py
@@ -11,6 +11,6 @@ pipe = pipeline(
 )
 
 # Specify the max_length parameter
-output = pipe("SEEU STUDENT QUESTION: What is probation?\nSEEU ADMINISTRATION ANSWER:", max_length=500)  # Adjust max_length as needed
+output = pipe("SEEU STUDENT QUESTION: What measures does the Security Service take to ensure the safety and well-being of residents in dormitories/housing facilities?\nSEEU ADMINISTRATION ANSWER:", max_length=500)  # Adjust max_length as needed
 
 print(output)
